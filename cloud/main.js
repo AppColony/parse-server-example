b8bc64fc16a40c5340585da5662ea3af2e63add0
Aaron Glenn <ag@aaronglenn.ca>
2017-01-06 15:00:40 -0800

    Relative requires

--- cloud/main.js
+++ cloud/main.js
@@ -1,13 +1,13 @@
-require('cloud/Driving.js');
-require('cloud/POFriendRelation.js');
-require('cloud/POFriendRequest.js');
-require('cloud/POPublicUser.js');
-require('cloud/POTrip.js');
-require('cloud/Promotion.js');
-require('cloud/Stats.js');
-require('cloud/Totals.js');
-require('cloud/CommunityTotals.js');
-require('cloud/Community.js');
-require('cloud/User.js');
-require('cloud/Installation.js'); 
+require('./cloud/Driving.js');
+require('./cloud/POFriendRelation.js');
+require('./cloud/POFriendRequest.js');
+require('./cloud/POPublicUser.js');
+require('./cloud/POTrip.js');
+require('./cloud/Promotion.js');
+require('./cloud/Stats.js');
+require('./cloud/Totals.js');
+require('./cloud/CommunityTotals.js');
+require('./cloud/Community.js');
+require('./cloud/User.js');
+require('./cloud/Installation.js');
 
