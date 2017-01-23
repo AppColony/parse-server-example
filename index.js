e2d1706eef358e266b0a5212d98f272de249b9ee
Nathan Willson <nwillson@AppColonys-MacBook-Pro-3.local>
2017-01-23 14:11:44 -0800

    Enabled VERBOSE logging, commented out Morgon (for now)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // compatible API routes.
 
 var express = require('express');
-var morgan = require('morgan');
+// var morgan = require('morgan');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
@@ -16,6 +16,7 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
+  verbose: process.env.VERBOSE || false,
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
@@ -33,7 +34,7 @@ var api = new ParseServer({
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
